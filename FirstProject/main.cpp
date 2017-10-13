2628c671450d55068e8152a45fa8bfb4416e69d5
Anton <anton@dyn-10-12-40-88.cad.ntu-kpi.kiev.ua>
2017-10-13 16:53:24 +0300

    Initial Commit

---
+++ FirstProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstProject
+//
+//  Created by Anton on 10/13/17.
+//  Copyright © 2017 Anton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
