97a03725f40d8fa6ff70d875a437ebc01bbf37da
Anton <anton@dyn-10-12-40-88.cad.ntu-kpi.kiev.ua>
2017-10-13 19:33:30 +0300

    result 2

--- FirstProject/main.cpp
+++ FirstProject/main.cpp
@@ -4,19 +4,44 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    double a, b, c, t, x, y;
-    printf("Enter a=");
-    scanf("%lf", &a);
+//    Лабараторная 1
+//    double a, b, c, t, x, y;
+//    printf("Enter a=");
+//    scanf("%lf", &a);
+//
+//    printf("Enter b=");
+//    scanf("%lf", &b);
+//
+//    printf("Enter c=");
+//    scanf("%lf", &c);
+//
+//    printf("Enter t=");
+//    scanf("%lf", &t);
+//
+//    printf("Your result x=%f\n", x = exp(-a*b) + cos(b));
+//    printf("Your result y=%f\n", y = b * cos(t * sqrt(c) + b));
     
-    printf("Enter b=");
+//    Лабараторная 2
+    double a, b, x1 = 0.4, x2 = 2, dx = 0.2, y;
+    
+    printf("Enter a = ");
+    scanf("%lf", &a);
+
+    printf("Enter b = ");
     scanf("%lf", &b);
     
-    printf("Enter c=");
-    scanf("%lf", &c);
-    
-    printf("Enter t=");
-    scanf("%lf", &t);
-    
-    printf("Your result x=%f\n", x = exp(-a*b) + cos(b));
-    printf("Your result y=%f\n", y = b * cos(t * sqrt(c) + b));
+    for ( ; x1 <= x2; x1 += dx ) {
+        if (x1 >= 1 && x1 <= 2) {
+            y = a * pow(a * x1, 2) * log(x1);
+            printf("x = %f\t y = %f\n", x1, y);
+        }
+        else if (x1 < 1) {
+            y = 1;
+            printf("x = %f\t y = %f\n", x1, y);
+        }
+        else if (x1 > 2) {
+            y = exp(a*x1) * cos(b*x1);
+            printf("x = %f\t y = %f\n", x1, y);
+        }
+    }
 }
