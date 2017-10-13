7b95f418857e7d46423dd484b3f8427afd95f076
Anton <anton@dyn-10-12-40-88.cad.ntu-kpi.kiev.ua>
2017-10-13 17:58:24 +0300

    first example

--- FirstProject/main.cpp
+++ FirstProject/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <stdio.h>
+#include <cmath>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    printf("Hello!\n");
-    std::cout << "Hello, World!\n";
-    return 0;
+    double a, b, c, t, x, y;
+    printf("Enter a=");
+    scanf("%lf", &a);
+    
+    printf("Enter b=");
+    scanf("%lf", &b);
+    
+    printf("Enter c=");
+    scanf("%lf", &c);
+    
+    printf("Enter t=");
+    scanf("%lf", &t);
+    
+    printf("Your result x=%f\n", x = exp(-a*b) + cos(b));
+    printf("Your result y=%f\n", y = b * cos(t * sqrt(c) + b));
 }
