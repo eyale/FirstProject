99b7de2c4ab36d544a0862adeb826e9494b2df83
Anton <anton@dyn-10-12-40-84.cad.ntu-kpi.kiev.ua>
2017-10-20 16:48:53 +0300

    adding the third lab

--- FirstProject/main.cpp
+++ FirstProject/main.cpp
@@ -22,26 +22,41 @@ int main(int argc, const char * argv[]) {
 //    printf("Your result y=%f\n", y = b * cos(t * sqrt(c) + b));
     
 //    Лабараторная 2
-    double a, b, x1 = 0.4, x2 = 2, dx = 0.2, y;
+//    double a, b, x1 = 0.4, x2 = 2, dx = 0.2, y;
+//
+//    printf("Enter a = ");
+//    scanf("%lf", &a);
+//
+//    printf("Enter b = ");
+//    scanf("%lf", &b);
+//
+//    for ( ; x1 <= x2; x1 += dx ) {
+//        if (x1 >= 1 && x1 <= 2) {
+//            y = a * pow(a * x1, 2) * log(x1);
+//            printf("x = %f\t y = %f\n", x1, y);
+//        }
+//        else if (x1 < 1) {
+//            y = 1;
+//            printf("x = %f\t y = %f\n", x1, y);
+//        }
+//        else if (x1 > 2) {
+//            y = exp(a*x1) * cos(b*x1);
+//            printf("x = %f\t y = %f\n", x1, y);
+//        }
+//    }
+    
+    //    Лабараторная 2
+    double a, x, n = 5;
     
     printf("Enter a = ");
     scanf("%lf", &a);
-
-    printf("Enter b = ");
-    scanf("%lf", &b);
     
-    for ( ; x1 <= x2; x1 += dx ) {
-        if (x1 >= 1 && x1 <= 2) {
-            y = a * pow(a * x1, 2) * log(x1);
-            printf("x = %f\t y = %f\n", x1, y);
-        }
-        else if (x1 < 1) {
-            y = 1;
-            printf("x = %f\t y = %f\n", x1, y);
-        }
-        else if (x1 > 2) {
-            y = exp(a*x1) * cos(b*x1);
-            printf("x = %f\t y = %f\n", x1, y);
-        }
+    printf("Enter x = ");
+    scanf("%lf", &x);
+    
+    for ( ; x <= n; x++ ) {
+        x+=a;
+        x*=x;
+        printf("x = %f\t a = %f\n", x, a);
     }
 }
